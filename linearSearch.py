38eceaceb60e22a506706ac1d5b72b5eb92168d0
Rahul Ganesh Sawant <rahulsawant8433@gmail.com>
2024-01-22 20:40:15 +0000

    initial commit

---
+++ linearSearch.py
@@ -0,0 +1,17 @@
+numbers = [10,20,30,40,50,60]
+choice =  int(input('Enter the number you want to search '))
+
+
+def linear_Search(numbers,c):
+    for i in range(len(numbers)):
+        if numbers[i] == c:
+            print('Number found at index: ',i)
+            return i
+        
+    print('Number not found')
+    return -1
+
+
+
+
+index =  linear_Search(numbers,choice)
